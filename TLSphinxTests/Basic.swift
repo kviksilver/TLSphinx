c6325cf63c8186fbbea6b32e849a14b94b8edfae
kviksilver <berceg@gmail.com>
2015-11-08 15:03:18 -0500

    [swift2] update more tests to swift2 syntax

--- TLSphinxTests/Basic.swift
+++ TLSphinxTests/Basic.swift
@@ -10,94 +10,81 @@ import UIKit
 import XCTest
 import TLSphinx
 
+extension XCTestCase {
+    
+    func getModelPath() -> NSURL? {
+        if let path = NSBundle(forClass: self.dynamicType).pathForResource("en-us", ofType: nil) {
+            return NSURL(string:path)
+        }
+        return nil
+    }
+    
+    func modelPathWithExtension(pathComponent: String) -> String? {
+        if let modelPath = getModelPath() {
+            return modelPath.URLByAppendingPathComponent(pathComponent).path
+        } else {
+            return nil
+        }
+    }
+}
+
 
 class BasicTests: XCTestCase {
     
-    func getModelPath() -> String? {
-        return NSBundle(forClass: BasicTests.self).pathForResource("en-us", ofType: nil)
-    }
-    
     func testConfig() {
-        
-        if let modelPath = getModelPath() {
-            
-            let hmm = modelPath.stringByAppendingPathComponent("en-us")
-            let lm = modelPath.stringByAppendingPathComponent("en-us.lm.dmp")
-            let dict = modelPath.stringByAppendingPathComponent("cmudict-en-us.dict")
-            
-            let config = Config(args: ("-hmm", hmm), ("-lm", lm), ("-dict", dict))
-            
-            XCTAssert(config != nil, "Pass")
-            
-        } else {
-            XCTFail("Can't access pocketsphinx model. Bundle root: \(NSBundle.mainBundle())")
+        guard let hmm = modelPathWithExtension("en-us"),
+            let lm = modelPathWithExtension("en-us.lm.dmp"),
+            let dict = modelPathWithExtension("cmudict-en-us.dict") else {
+                XCTFail("Can't access pocketsphinx model. Bundle root: \(NSBundle.mainBundle())")
+                return
         }
+        let config = Config(args: ("-hmm", hmm), ("-lm", lm), ("-dict", dict))
+        XCTAssert(config != nil, "Pass")
     }
     
     func testDecoder() {
         
-        if let modelPath = getModelPath() {
-            
-            let hmm = modelPath.stringByAppendingPathComponent("en-us")
-            let lm = modelPath.stringByAppendingPathComponent("en-us.lm.dmp")
-            let dict = modelPath.stringByAppendingPathComponent("cmudict-en-us.dict")
-            
-            if let config = Config(args: ("-hmm", hmm), ("-lm", lm), ("-dict", dict)) {
-                let decoder = Decoder(config:config)
-                
-                XCTAssert(decoder != nil, "Pass")
-            } else {
-                XCTFail("Can't run test without a valid config")
-            }
-            
-        } else {
-            XCTFail("Can't access pocketsphinx model. Bundle root: \(NSBundle.mainBundle())")
+        guard let hmm = modelPathWithExtension("en-us"),
+            let lm = modelPathWithExtension("en-us.lm.dmp"),
+            let dict = modelPathWithExtension("cmudict-en-us.dict"),
+            let config = Config(args: ("-hmm", hmm), ("-lm", lm), ("-dict", dict)) else {
+                XCTFail("Can't access pocketsphinx model. Bundle root: \(NSBundle.mainBundle())")
+                return
         }
+        
+        let decoder = Decoder(config:config)
+        XCTAssert(decoder != nil, "Pass")
+        
     }
     
     func testSpeechFromFile() {
         
-        if let modelPath = getModelPath() {
-            
-            let hmm = modelPath.stringByAppendingPathComponent("en-us")
-            let lm = modelPath.stringByAppendingPathComponent("en-us.lm.dmp")
-            let dict = modelPath.stringByAppendingPathComponent("cmudict-en-us.dict")
-            
-            if let config = Config(args: ("-hmm", hmm), ("-lm", lm), ("-dict", dict)) {
-                if let decoder = Decoder(config:config) {
-                    
-                    let audioFile = modelPath.stringByAppendingPathComponent("goforward.raw")
-                    let expectation = expectationWithDescription("Decode finish")
-                    
-                    decoder.decodeSpeechAtPath(audioFile) {
-                        
-                        if let hyp = $0 {
-                            
-                            println("Text: \(hyp.text) - Score: \(hyp.score)")
-                            XCTAssert(hyp.text == "go forward ten meters", "Pass")
-                            
-                        } else {
-                            XCTFail("Fail to decode audio")
-                        }
-                        
-                        expectation.fulfill()
-                    }
-                    
-                    waitForExpectationsWithTimeout(NSTimeIntervalSince1970, handler: { (_) -> Void in
-                        
-                    })
-                    
-                } else {
-                    XCTFail("Can't run test without a decoder")
-                }
-                
-            } else {
-                XCTFail("Can't run test without a valid config")
-            }
-            
-        } else {
-            XCTFail("Can't access pocketsphinx model. Bundle root: \(NSBundle.mainBundle())")
+        guard let hmm = modelPathWithExtension("en-us"),
+            let lm = modelPathWithExtension("en-us.lm.dmp"),
+            let dict = modelPathWithExtension("cmudict-en-us.dict"),
+            let config = Config(args: ("-hmm", hmm), ("-lm", lm), ("-dict", dict)),
+            let decoder = Decoder(config:config) else {
+                XCTFail("Can't access pocketsphinx model. Bundle root: \(NSBundle.mainBundle())")
+                return
         }
+        
+        let audioFile = modelPathWithExtension("goforward.raw")
+        let expectation = expectationWithDescription("Decode finish")
+        
+        decoder.decodeSpeechAtPath(audioFile!) {
+            if let hyp = $0 {
+                print("Text: \(hyp.text) - Score: \(hyp.score)")
+                XCTAssert(hyp.text == "go forward ten meters", "Pass")
+            } else {
+                XCTFail("Fail to decode audio")
+            }
+            expectation.fulfill()
+        }
+        
+        waitForExpectationsWithTimeout(NSTimeIntervalSince1970, handler: { (_) -> Void in
+            
+        })
+        
         
     }
 }
--- TLSphinxTests/LiveDecode.swift
+++ TLSphinxTests/LiveDecode.swift
@@ -13,46 +13,37 @@ import TLSphinx
 
 class LiveDecode: XCTestCase {
     
-    func getModelPath() -> String? {
-        return NSBundle(forClass: LiveDecode.self).pathForResource("en-us", ofType: nil)
-    }
-
     func testAVAudioRecorder() {
         
-        if let modelPath = getModelPath() {
-            
-            let hmm = modelPath.stringByAppendingPathComponent("en-us")
-            let lm = modelPath.stringByAppendingPathComponent("en-us.lm.dmp")
-            let dict = modelPath.stringByAppendingPathComponent("cmudict-en-us.dict")
-            
-            if let config = Config(args: ("-hmm", hmm), ("-lm", lm), ("-dict", dict)) {
-                
-                config.showDebugInfo = false
-                
-                if let decoder = Decoder(config:config) {
-                    decoder.startDecodingSpeech { (hyp) -> () in
-                        println("Utterance: \(hyp)")
-                    }
-                    
-                    let expectation = expectationWithDescription("")
-                    
-                    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, Int64(5.0 * Double(NSEC_PER_SEC))) , dispatch_get_main_queue(), { () -> Void in
-                        decoder.stopDecodingSpeech()
-                        expectation.fulfill()
-                    })
-                    
-                    waitForExpectationsWithTimeout(NSTimeIntervalSince1970, handler: { (error: NSError!) -> Void in
-                        
-                    })
-                }
-                
-            } else {
-                XCTFail("Can't run test without a valid config")
+        guard let hmm = modelPathWithExtension("en-us"),
+            let lm = modelPathWithExtension("en-us.lm.dmp"),
+            let dict = modelPathWithExtension("cmudict-en-us.dict"),
+            let config = Config(args: ("-hmm", hmm), ("-lm", lm), ("-dict", dict)) else {
+                XCTFail("Can't access pocketsphinx model. Bundle root: \(NSBundle.mainBundle())")
+                return
+        }
+        
+        config.showDebugInfo = false
+        
+        if let decoder = Decoder(config:config) {
+            decoder.startDecodingSpeech { (hyp) -> () in
+                print("Utterance: \(hyp)")
             }
             
-        } else {
-            XCTFail("Can't access pocketsphinx model. Bundle root: \(NSBundle.mainBundle())")
+            let expectation = expectationWithDescription("")
+            
+            dispatch_after(dispatch_time(DISPATCH_TIME_NOW, Int64(5.0 * Double(NSEC_PER_SEC))) , dispatch_get_main_queue(), { () -> Void in
+                decoder.stopDecodingSpeech()
+                expectation.fulfill()
+            })
+            
+            waitForExpectationsWithTimeout(NSTimeIntervalSince1970, handler: { (_) -> Void in
+                
+            })
+        }
+        
+        
+        
         }
     }
 
-}
